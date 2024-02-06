ea026d91095c53ef4193bf994097dbb3fc1b691f
Jia Wang <daisy-a.119@hotmail.com>
2024-02-05 23:35:25 -0500

    render card display

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 // npm modules
 import { Route, Routes } from "react-router-dom"
+import { useState, useEffect } from 'react'
 
 //pages
 import StarshipsCards from "./pages/StarshipsCards/StarshipsCards";
+
+// services
+import *  as swService from './services/sw-api'
 
 // css
 import './App.css'
 
 const App = () => {
+
+  const [starships, setStarships] = useState([])
+
+  useEffect(() => {
+    const fetchStarships = async() =>{
+      const data =await swService.getAllStarships()
+      // console.log("response from api:", data)
+      setStarships(data.results)
+    }
+    fetchStarships()
+  },[])
+
   return ( 
-    <>
+    <main>
     <Routes>
-    <Route path='/' element={<StarshipsCards />} />
+    <Route path='/' element={
+      <StarshipsCards starships={starships} />
+      } />
     </Routes>
-    </>
+    </main>
   );
 }
 export default App;
--- src/pages/StarshipsCards/StarshipsCards.jsx
+++ src/pages/StarshipsCards/StarshipsCards.jsx
@@ -1,7 +1,19 @@
-const starshipsCards = () => {
+import React from 'react'
+
+// css
+import './StarshipsCards.css'
+
+const starshipsCards = (props) => {
   return (  
     <main>
-      <h1>Starships</h1>
+      <h1>Starwar Starships</h1>
+      <div id="card-container">
+      {
+        props.starships.map( ship =>
+          <div className='green-btn'>{ship.name}</div>
+        )
+      }
+      </div>
     </main>
   );
 }
